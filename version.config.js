0a3eca4272d58b78d08f8ab4dcd45b0f9dccec17
theredstonee <blitz.der.wahrheit@gmail.com>
2025-12-04 14:48:15 +0100

    chore: bump version to 1.5.3 with changelog
    New features in v1.5.3:
    - Auto-Close system with cancel button
    - Time configurable in hours (25-720h)
    - Warning 24h before closing
    - Message statistics on ticket close
    - All features now free (only Whitelabel premium)

--- version.config.js
+++ version.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  VERSION: '1.5.2',
-  RELEASE_DATE: '2025-12-01',
+  VERSION: '1.5.3',
+  RELEASE_DATE: '2025-12-04',
   AUTHOR: 'Theredstonee',
   REPOSITORY: 'https://github.com/Theredstonee/TRS-Tickets-Bot',
   SUPPORT_SERVER: 'https://discord.gg/theredstonee',
